2545cd8cca0997a34a5f393ca9d00dab4bf2b562
zhuhuijun <zhuhjtcx@qq.com>
2017-01-07 21:46:53 +0800

    use stack

---
+++ 20170107usestack/DemoUseStack/DemoUseStack/Demo02_中缀表达式.c
@@ -0,0 +1,12 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "linkstack.h"
+/************************************************************************/
+/* 中缀表达式转化为后缀表达式                                                                     */
+/************************************************************************/
+void main001(int argc,char* argv[])
+{
+	printf("hello,world!\n");
+	system("pause");
+	return;
+}
