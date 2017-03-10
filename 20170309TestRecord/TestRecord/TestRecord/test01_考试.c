73214e652f8c1ec23647cd4e58391e3bebc4dc31
zhuhuijun <zhuhjtcx@qq.com>
2017-03-10 08:17:25 +0800

    fg

--- 20170309TestRecord/TestRecord/TestRecord/test01_考试.c
+++ 20170309TestRecord/TestRecord/TestRecord/test01_考试.c
@@ -1,81 +1,96 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-/************************************************************************/
-/* 排序的算法                                                                     */
-/************************************************************************/
-char ** sortArray(char *pArray1,int nArrayNum,char (*parry2)[30],int nArraynum2,int *narray3num)
+
+//10 + 10 = 20
+char **sortArray1Array2_01(char **pArray1, int nArray1Num, char (*pArray2)[30], int nArray2Num,  int *nArray3Num)
 {
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	int leng = nArrayNum + nArraynum2;
-	char tmp[30]={0};
-	char **pTemp=(char **)malloc(leng*sizeof(char *));
-	for (i=0; i<leng; i++)
+	//第一维
+	int length=nArray1Num+nArray2Num;
+	int i=0,j=0; int k = 0;
+
+	char **pTmp = (char **)malloc(  (nArray1Num+nArray2Num)*sizeof(char *));
+
+	for (i=0; i<length; i++)
 	{
-		pTemp[i] = (char *)malloc( sizeof(char) * 30);
-		memset(pTemp[i] , 0 , sizeof(char) * 30);
-	}
-	for(i = 0; i<nArrayNum; i++){
-		strcpy(pTemp[i],pArray1[i]);
-	}
-	for (k = 0;k<nArraynum2; i++, k++){
-		strcpy(pTemp[i],parry2[k]);
+		pTmp[i] = (char*)malloc(sizeof(char)*30);
+		memset(pTmp[i], 0, sizeof(char)*30   );
 	}
 
-	for (i = 0; i<leng; i++)
+	for (i=0;i<nArray1Num;i++)
 	{
-		for (j=i+1; j < leng; j++){
-			if(strcmp(pTemp[i],pTemp[j]) > 0){
-				strcpy(tmp,pTemp[i]);
-				strcpy (pTemp[i],pTemp[j]);
-				strcpy (pTemp[j] , tmp);
+		strcpy(pTmp[i], pArray1[i]);	
+	}
+
+	for (k=0; k<nArray2Num; i++,k++)
+	{
+		strcpy(pTmp[i], pArray2[k]);
+	}
+
+	for (i=0;i<length;i++)
+	{
+		for (j=i+1;j<length;j++)
+		{
+			if(strcmp(pTmp[i],pTmp[j])>0)
+			{
+				char tmp[30]={0};
+				strcpy(tmp,pTmp[i]);
+				strcpy(pTmp[i],pTmp[j]);
+				strcpy(pTmp[j],tmp);
 			}
 		}
 	}
-	*narray3num = leng;
+	*nArray3Num=length;
 
-	return pTemp;
+	return pTmp;
+
 }
-
-void free_arr(char** arr,int len){
+void free_arr01(char** arr,int length)
+{
 	int i=0;
-	if(arr = NULL){
+	if(arr==NULL)
+	{
 		return ;
 	}
-	for (i=0; i <len; i++)
+	for(i=0;i<length;i++)
 	{
-		if (arr[i] != NULL){
-			free (arr[i]);
+		if(arr[i]!=NULL)
+		{
+			free(arr[i]);
 		}
 	}
-	if (arr != NULL){
+	if(arr!=NULL)
+	{
 		free(arr);
 	}
 }
-
-int main(int argc,char* argv[])
+void main()
 {
-	int i=0;
+
+	int i = 0;
 	char **pArray3 = NULL;
-	int narray3Num = 0;
+	int nArray3Num = 0;
 	//指针数组
-	char* array[]={"bbbb","aaaa","cccc"};
+	char *array1[] = {"bbbbb", "aaaaa", "ccccc", "22222", "11111", "44444"};
+
 	//二维数组
-	char array2[10][30]={"zzzz","yyyy","3333"};
-	pArray3=sortArray(array,3,array2,3,&narray3Num);
-	if(pArray3 == NULL){
-		printf("func sortArray error \n");
-	}
-	for(i=0;i<narray3Num;i++){
-		printf("%s",pArray3[i]);
-	}
-	if(pArray3 != NULL)
+	char array2[10][30] = {"zzzz", "yyyy", "333333"};
+
+	pArray3 = sortArray1Array2_01(array1, 6, array2, 3,  &nArray3Num);
+	if (pArray3 == NULL)
 	{
-		free_arr(pArray3,narray3Num);
-		pArray3 = NULL;
+		printf("func sortArray1Array() err\n");
+	}
+
+	for (i=0; i<nArray3Num;i++)
+	{
+		printf("%s \n", pArray3[i]);
+	}
+	if(pArray3!=NULL)
+	{
+		free_arr01(pArray3,nArray3Num);
+		pArray3=NULL;
 	}
 	system("pause");
-	return 1;
+
 }
